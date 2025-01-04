a8cb0002b51bf5c6bd7eda87a5c5a122ffc44623
YEDIDA VINDHYA <137333165+01vindhya@users.noreply.github.com>
2025-01-04 14:19:57 +0530

    Update ProductRoutes.js

--- Backend/authRoutes/ProductRoutes.js
+++ Backend/authRoutes/ProductRoutes.js
@@ -4,13 +4,13 @@ const ProductController = require("../controllers/ProductController");
 const authMiddleware = require("../middleware/authMiddleware"); // Auth middleware for role-based access
 
 // GET /products - Get all products
-router.get("/", authMiddleware, ProductController.getProducts);
+router.get("/", ProductController.getProducts);
 
 // POST /products - Add a new product (Admin only)
 router.post("/", authMiddleware, ProductController.addProduct);
 
 // PUT /products/:id - Update product details
-router.put("/:id", authMiddleware, ProductController.updateProduct);
+router.put("/:id", ProductController.updateProduct);
 
 // DELETE /products/:id - Delete a product (Admin only)
 router.delete("/:id", authMiddleware, ProductController.deleteProduct);
